7a401a75c1414414f15d49df3e8b423f69fcde8f
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-12-22 20:30:10 +0400

    add code

--- code.py
+++ code.py
@@ -1,1 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+URL = "https://www.instagram.com/partling_/"
+def parse_data(s):
+    data = {}
+    s = s.split("-")[0]
+    s = s.split(" ")
+    data['Followers'] = s[0]
+    data['Following'] = s[2]
+    data['Posts'] = s[4]
+    return data
 
+def scrape_data(username):
+    r = requests.get(URL.format(username))
+    s = BeautifulSoup(r.text, "html.parser")
+    meta = s.find("meta", property ="og:description")
+    return parse_data(meta.attrs['content'])
+if __name__ == "__main__":
+    username = "python.hub"
+    data = scrape_data(username)
+    print("Followers ", data["Followers"])
+    print("Following ", data["Following"])
+    print("Posts ", data["Posts"])
